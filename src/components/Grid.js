77f2bde29392af2926a206a97ad6ab27f214c821
ymoukhli <ymoukhli.1337@gmail.com>
2022-06-14 16:46:19 +0100

    rerender only if needed @ ouf @

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,8 +1,7 @@
-import React, {useContext } from "react";
+import React, { useContext } from "react";
 import { gridContext } from "../context/gridContext";
 export default function Grid() {
   const [gridJSX] = useContext(gridContext);
-  console.log('setting Grid')
   return (
     <>
       <div id="table">
